f0bd82cc8e2d260d34a233d141a385741c332b77
FangLingxiao <lingxiaofong@outlook.com>
2024-07-24 14:56:08 +0100

    add ambience func

--- sync.py
+++ sync.py
@@ -1,6 +1,7 @@
 import classify as classify
 from PIL import Image
 import cv2
+from collections import Counter
 
 class ObjectIntervalSync:
     def __init__(self, video_path):
@@ -12,10 +13,16 @@ class ObjectIntervalSync:
         self.frame_scores = []
         self.object_intervals = {obj: [] for obj in classify.ESC_50_classes}
         self.object_status = {obj: {'in_interval': False, 'start_frame': None} for obj in classify.ESC_50_classes}
+        self.weather_counts = Counter()
+        self.place_counts = Counter()
+        self.scene_counts = Counter()
+        self.time_counts = Counter()
+        self.ambience = None
 
     def analyze_frames(self, frame_step=2):
         for i, frame in enumerate(self.resized_frame[::frame_step]):
             pil_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            image_tensor = self.classify.preprocess_image(pil_image)
 
             values, objects = self.classify.recognize_objects(pil_image)
             frame_score = {
@@ -25,6 +32,46 @@ class ObjectIntervalSync:
             self.frame_values.append(values)
             self.frame_objects.append(objects)
             self.frame_scores.append(frame_score)
+
+            # Classify weather, place, and scene
+            weather = self.classify.classify_weather(image_tensor)
+            place = self.classify.classify_place(image_tensor)
+            scene = self.classify.classify_scene(image_tensor)
+            scene = self.classify.classify_time(image_tensor)
+
+            self.weather_counts[weather] += 1
+            self.place_counts[place] += 1
+            self.scene_counts[scene] += 1
+            self.time_counts[scene] += 1
+
+        self.determine_ambience()
+
+    def determine_ambience(self):
+        weather = self.weather_counts.most_common(1)[0][0]
+        place = self.place_counts.most_common(1)[0][0]
+        scene = self.scene_counts.most_common(1)[0][0]
+        time = self.time_counts.most_common(1)[0][0]
+
+        if weather == "windy":
+            self.ambience = "windy"
+        elif weather == "thunderstorm":
+            self.ambience = "thunderstorm"
+        elif weather == "rainy":
+            self.ambience = "rainy"
+        elif weather == "drizzle":
+            self.ambience = "drizzle"
+        elif weather == "sunny":
+            if place == "nature":
+                if time == "day":
+                    self.ambience = "nature day"
+                else:
+                    self.ambience = "nature night"
+            elif place == "urban":
+                if scene == "indoors":
+                    self.ambience = "urban indoor"
+                else:
+                    self.ambience = "urban outdoor"
+
         
     def calculate_intervals(self, frame_rate=30):
         for frame_idx, (objects, scores) in enumerate(zip(self.frame_objects, self.frame_values)):
@@ -52,3 +99,6 @@ class ObjectIntervalSync:
     def get_intervals(self):
         self.object_intervals = {k: v for k, v in self.object_intervals.items() if v}
         return self.object_intervals
+    
+    def get_ambience(self):
+        return self.ambience
