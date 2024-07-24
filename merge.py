24f6132cb58f7c838e5b9c007f2c9ef691910a3c
FangLingxiao <lingxiaofong@outlook.com>
2024-07-24 21:55:48 +0100

    update

--- merge.py
+++ merge.py
@@ -8,7 +8,7 @@ def merge_audio_video(video_path, effect_infos, ambience_audio, output_path):
     """
     Merge video with multiple audios and output to the specified path.
 
-    参数:
+    parameters:
     video_path (str): path to video folder
     effect_infos (list): list of effect info:
         [
@@ -26,20 +26,7 @@ def merge_audio_video(video_path, effect_infos, ambience_audio, output_path):
     try:
         # load video
         video_clip = VideoFileClip(video_path)
-        
-        # load and edit audio
-        effect_clips = []
-        for effect_info in effect_infos:
-            effect_clip = AudioFileClip(effect_info['effect_file'])
-            effect_clip = effect_clip.subclip(0, min(effect_clip.duration, video_clip.duration))
-            effect_clips.append(effect_clip)
 
-        # Merge sound effects
-        if effect_clips:
-            effect_audio = concatenate_audioclips(effect_clips)
-        else:
-            effect_audio = None
-        
         # Process effect audio
         if ambience_audio:
             ambience_clip =  AudioFileClip(ambience_audio['ambience_file'])
@@ -48,19 +35,29 @@ def merge_audio_video(video_path, effect_infos, ambience_audio, output_path):
             # edit ambience volume
             volume_factor = 10**(-10/20) # convert dB to linear scale factor
             ambience_clip = adjust_volume(ambience_clip, volume_factor)
-
-            # Merge effect and ambience
-            if effect_audio:
-                final_audio = CompositeAudioClip([effect_audio, ambience_clip])
-            else:
-                final_audio = ambience_clip
-            
         else:
-            final_audio = effect_audio if effect_audio else None
+            ambience_clip = None
+        
+        # load and edit audio
+        effect_clips = []
+        for effect_info in effect_infos:
+            effect_clip = AudioFileClip(effect_info['effect_file'])
+            start_time = effect_info['interval'][0] / video_clip.fps
+            end_time = min(start_time + effect_clip.duration, video_clip.duration)
+            effect_clip = effect_clip.subclip(0, end_time - start_time)
+            effect_clips.append(effect_clip.set_start(start_time))
+
+        # Merge sound effects
+        all_audio_clips = effect_clips
+        if ambience_clip:
+            all_audio_clips.insert(0, ambience_clip)
+
+        final_audio = CompositeAudioClip(all_audio_clips)
+
+
         
         # Set final audio as audio for video
-        if final_audio:
-            video_clip = video_clip.set_audio(final_audio)
+        video_clip = video_clip.set_audio(final_audio)
 
         # Output        
         video_clip.write_videofile(output_path, codec='libx264', audio_codec='aac')
